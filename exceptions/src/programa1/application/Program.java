b6fde9f76be581ef45879e144d056756a38b90b7
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-10-09 18:41:18 -0300

    bad solution

--- exceptions/src/programa1/application/Program.java
+++ exceptions/src/programa1/application/Program.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-s
+
 public class Program {
 
     public static void main(String[] args) throws ParseException {
